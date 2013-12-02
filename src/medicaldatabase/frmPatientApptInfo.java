f7ca7435ff9aca1ea06a22a6720696f4e5aecab0
Chris Wendt <chrismwendt@gmail.com>
2013-12-02 15:22:42 -0500

    Widen patient schedule appointment button

--- src/medicaldatabase/frmPatientApptInfo.java
+++ src/medicaldatabase/frmPatientApptInfo.java
@@ -231,7 +231,7 @@ public class frmPatientApptInfo extends javax.swing.JFrame {
                         .addComponent(notes, javax.swing.GroupLayout.PREFERRED_SIZE, 166, javax.swing.GroupLayout.PREFERRED_SIZE)
                         .addPreferredGap(javax.swing.LayoutStyle.ComponentPlacement.RELATED, javax.swing.GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE)
                         .addGroup(layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-                            .addComponent(btnSchedule, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE)
+                            .addComponent(btnSchedule, javax.swing.GroupLayout.PREFERRED_SIZE, 120, javax.swing.GroupLayout.PREFERRED_SIZE)
                             .addComponent(btnCancel, javax.swing.GroupLayout.PREFERRED_SIZE, 79, javax.swing.GroupLayout.PREFERRED_SIZE))
                         .addGap(25, 25, 25))))
         );
